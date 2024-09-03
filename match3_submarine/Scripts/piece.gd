4877cc41ddbd1c0ba7a59a3c35295d0dd441481d
livinwater <livinoffwater@gmail.com>
2024-09-04 00:55:33 +0800

    Initalize project

---
+++ match3_submarine/Scripts/piece.gd
@@ -0,0 +1,21 @@
+extends Control
+
+@export var color: String
+
+var matched = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	pass # Replace with function body.
+
+func move(target):
+	var tween: Tween = create_tween()
+	tween.tween_property(self,"position",target,0.3).set_trans(Tween.TRANS_BOUNCE).set_ease(Tween.EASE_OUT)
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func dim():
+	get_node("Sprite")
